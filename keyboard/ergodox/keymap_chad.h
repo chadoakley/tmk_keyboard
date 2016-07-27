add3ce36ba2b2e4d57092e8fcf15cd241958e505
Chad Oakley <coakley@razorvision.net>
2016-07-27 12:08:11 -0500

    Updated chad layout

--- keyboard/ergodox/keymap_chad.h
+++ keyboard/ergodox/keymap_chad.h
@@ -9,7 +9,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         NO,  NO,  NO,  LALT,LGUI,
                                       FN25,FN26,
                                            NO,
-                                 FN1, FN2, FN30,
+                                 FN1, LCTL,FN30,
         // right hand
              NO,  6,   7,   8,   9,   0,   MINS,
              FN8, Y,   U,   I,   O,   P,   EQL,
@@ -23,19 +23,19 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KEYMAP(  // Layer1: NUMPAD
         // left hand
-        TRNS,NO,  NO,  NO,  NO,  NO,  NO,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-        TRNS,NO,  NO,  NO,  NO,  NO,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
                                            TRNS,
                                  TRNS,TRNS,TRNS,
         // right hand
              NO,  NO,  PSLS,PAST,PAST,PMNS,BSPC,
-             TRNS,NO,  P7,  P8,  P9,  PMNS,PGUP,
+             NO,  NO,  P7,  P8,  P9,  PMNS,PGUP,
                   NO,  P4,  P5,  P6,  PPLS,PGDN,
-             TRNS,NO,  P1,  P2,  P3,  PPLS,PENT,
+             NO,  NO,  P1,  P2,  P3,  PPLS,PENT,
                        P0,  PDOT,SLSH,PENT,PENT,
         TRNS,TRNS,
         TRNS,
@@ -44,10 +44,10 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KEYMAP(  // Layer2: SPECIAL RIGHT
         // left hand
-        TRNS,NO,  NO,  NO,  NO,  NO,  NO,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
-        TRNS,NO,  NO,  NO,  NO,  NO,
-        TRNS,NO,  NO,  NO,  NO,  NO,  TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
                                            TRNS,
@@ -92,19 +92,19 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KEYMAP(  // Layer4: F-keys only
         // left hand
-        TRNS,NO,  NO,  NO,  NO,  NO,  NO,
-        TRNS,F13, F14, F15, F16, NO,  TRNS,
-        TRNS,F17, F18, F19, F20, NO,
-        TRNS,F21, F22, F23, F24, NO,  TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,F13, F14, F15, F16, TRNS,TRNS,
+        TRNS,F17, F18, F19, F20, TRNS,
+        TRNS,F21, F22, F23, F24, TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
                                            TRNS,
                                  TRNS,TRNS,TRNS,
         // right hand
-             NO,  NO,  NO,  NO,  NO,  NO,  TRNS,
-             TRNS,NO,  F1,  F2,  F3,  F4,  TRNS,
-                  NO,  F5,  F6,  F7,  F8,  TRNS,
-             TRNS,NO,  F9,  F10, F11, F12, TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+             TRNS,TRNS,F1,  F2,  F3,  F4,  TRNS,
+                  TRNS,F5,  F6,  F7,  F8,  TRNS,
+             TRNS,TRNS,F9,  F10, F11, F12, TRNS,
                        TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,
         TRNS,
@@ -113,14 +113,14 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KEYMAP(  // Layer5: Gaming?
         // left hand
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        F1,  F2,  F3,  F4,  F5,  F6,  F7,
+        TAB, TRNS,TRNS,TRNS,TRNS,TRNS,F8,
+        ESC, TRNS,TRNS,TRNS,TRNS,TRNS,
+        LSFT,TRNS,TRNS,TRNS,TRNS,TRNS,F9,
         TRNS,TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
-                                           TRNS,
-                                 SPC, TRNS,TRNS,
+                                           F10,
+                                 FN1, LCTL,LALT,
         // right hand
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
@@ -141,24 +141,24 @@ enum macro_id {
  * Fn action definition
  */
 static const uint16_t PROGMEM fn_actions[] = {
-    [1] =  ACTION_MODS_TAP_KEY(MOD_LSFT, KC_BSPC),         // FN1 = LShift with tap BackSpace
-    [2] =  ACTION_MODS_TAP_KEY(MOD_LCTL, KC_DEL),          // FN2 = LCtrl  with tap Delete
+    [1] =   ACTION_MODS_TAP_KEY(MOD_LSFT, KC_BSPC),         // FN1 = LShift with tap BackSpace
+    [2] =   ACTION_MODS_TAP_KEY(MOD_LCTL, KC_DEL),          // FN2 = LCtrl  with tap Delete
 
-    [3] =  ACTION_MODS_TAP_KEY(MOD_RCTL, KC_ENT),          // FN3 = UNUSED
-    [4] =  ACTION_MODS_TAP_KEY(MOD_RSFT, KC_SPC),          // FN4 = UNUSED
+    [3] =   ACTION_MODS_TAP_KEY(MOD_RCTL, KC_ENT),          // FN3 = UNUSED
+    [4] =   ACTION_MODS_TAP_KEY(MOD_RSFT, KC_SPC),          // FN4 = UNUSED
 
 
-    [8] =  ACTION_MODS_KEY(MOD_LCTL | MOD_LSFT, KC_SPC),   // FN8 = Moom
-    [9] =  ACTION_MODS_KEY(MOD_LCTL | MOD_LGUI, KC_SPC),   // FN9 = Alfred
+    [8] =   ACTION_MODS_KEY(MOD_LCTL | MOD_LSFT, KC_SPC),   // FN8 = Alfred
+    [9] =   ACTION_MODS_KEY(MOD_LCTL | MOD_LGUI, KC_SPC),   // FN9 = Moom
 
-    [25] = ACTION_LAYER_SET(0, ON_BOTH),                   // FN25 - set Layer0
-    [26] = ACTION_LAYER_SET(5, ON_BOTH),                   // FN26 - set Layer5
+    [25] =  ACTION_LAYER_SET(0, ON_BOTH),                   // FN25 - set Layer0
+    [26] =  ACTION_LAYER_TOGGLE(5),                         // FN26 - set Layer5
 
-    [28] =  ACTION_LAYER_MOMENTARY(1),                     // FN28 = momentary NUMPAD
-    [29] =  ACTION_LAYER_MOMENTARY(2),                     // FN20 = momentary SPECIAL KEYS
-    [30] =  ACTION_LAYER_MOMENTARY(3),                     // FN30 = momentary ARROWS
-    [31] =  ACTION_LAYER_MOMENTARY(4),                     // FN31 = momentary FUNCTION KEYS
-};
+    [28] =  ACTION_LAYER_MOMENTARY(1),                      // FN28 = momentary NUMPAD
+    [29] =  ACTION_LAYER_MOMENTARY(2),                      // FN20 = momentary SPECIAL KEYS
+    [30] =  ACTION_LAYER_MOMENTARY(3),                      // FN30 = momentary ARROWS
+    [31] =  ACTION_LAYER_MOMENTARY(4),                      // FN31 = momentary FUNCTION KEYS
+} ;
 static const uint16_t PROGMEM fn_actions_1[] = {
 };
 static const uint16_t PROGMEM fn_actions_2[] = {
@@ -179,6 +179,7 @@ static const uint16_t PROGMEM fn_actions_3[] = {
 static const uint16_t PROGMEM fn_actions_4[] = {
 };
 static const uint16_t PROGMEM fn_actions_5[] = {
+    [1] =   ACTION_MODS_TAP_KEY(SPC, KC_BSPC),         // FN1 = LShift with tap BackSpace
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
